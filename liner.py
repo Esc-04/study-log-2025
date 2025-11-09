19cedb90d9a2a6f69e28c049428f4d041f6026a4
Esc-04 <escho94@naver.com>
2025-11-09 15:44:38 +0900

    4단계 코드 추가

--- liner.py
+++ liner.py
@@ -6,7 +6,13 @@
 # 4단계 : 나머지 경우는 행렬을 가우스-조던 소거법으로 진행
 # 5단계 : 자유변수로 무수히 많은 해 표현하기
 import sys
-
+#소숫점 이쁘게 표현
+def fmt(num):
+    if abs(num - int(num)) < 1e-9:
+        return str(int(num))
+    else:
+        return f"{num:.3f}"
+    
 #2단계
 def det(A):     #3x3 행렬식 구하기
     res=0
@@ -33,28 +39,25 @@ def gauss(A):
             continue
         A[c],A[n]=A[n],A[c] #행교환
         A[c]=[num/pivot for num in A[c]] #피벗을 1로 만들기
-        for m in A:
-            print(m)
+        
         # c 현재 열
         for row in range(3):
             if row!=c and A[row][c]!=0: #현재 행이거나 0일경우 빼고 연산
                 k=A[row][c]
                 for j in range(4):      #기본행 연산
                     A[row][j]-=k*A[c][j]
-                for m in A:
-                    print(m)
-    for m in A:
-        print(m)
+
     #해 없음 판단
     for a in A:
         if not any(a[:3]) and a[3]!=0:
             print("해가 없습니다")
             break
-        #무수히 많은 해 일때
-        else:
-            print("무수히 많은 해를 가집니다.")
-            break
-            pass #자유변수
+    #무수히 많은 해
+    else:
+        print("무수히 많은 해를 가집니다.")
+        return True
+    return False
+        
 
 #1단계
 def solve_matrix():
@@ -93,13 +96,40 @@ def solve_matrix():
         t2=(y[1]-U[1][2]*t3)/U[1][1]
         t1=(y[0]-U[0][1]*t2-U[0][2]*t3)/U[0][0]
         
-        print(f"해: x={t1}, y={t2}, z={t3}")
+        print(f"해: x={fmt(t1)}, y={fmt(t2)}, z={fmt(t3)}")
     
     #4단계
     else:
         #첨가행렬
         A=[A[i]+[b[i]] for i in range(3)]
-        gauss(A)
+        if gauss(A): #무수히 많은  해를 가진다면
+            pivot_cols = []
+            for r in range(3):
+                for c in range(3):
+                    # 행 전체가 0이 아니고, c열 값이 1이고 그 왼쪽은 다 0이면 피벗
+                    if A[r][c] == 1 and all(A[r][k] == 0 for k in range(c)):
+                        pivot_cols.append(c)
+                        break
+            free_cols = [i for i in range(3) if i not in pivot_cols]
+            ans=[0,0,0]
+            for f in free_cols:
+                ans[f]=chr(ord('s')+f)
+            for row in range(3):
+                for col in range(3):
+                    if col in pivot_cols and A[row][col]==1:
+                        ex=str(A[row][3]) #일단 상수항
+                        for f in free_cols:
+                            if A[row][f]==1:
+                                ex+=f"-{ans[f]}"
+                            elif A[row][f]==-1:
+                                ex+=f"+{ans[f]}"
+                            elif A[row][f]==0:
+                                continue
+                            else:
+                                ex+=f"{-A[row][f]}{ans[f]}"
+                        ans[col]=ex
+            print(f"x={fmt(ans[0])} y={fmt(ans[1])} z={fmt(ans[2])}")
+
 
 
 print("과제를 몇번 테스트 하시겠습니까?")
